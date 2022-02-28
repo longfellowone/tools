b2dd65369b4c95d25b9de156e08a9788c55104da
Matt Wright <mattwright3195@gmail.com>
2022-02-27 19:55:24 -0800

    re add preconnect

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter&display=option"
           rel="stylesheet"
         />
-        {/* <link rel="preconnect" href={process.env.NEXT_PUBLIC_GRAPHQL_URL} crossOrigin="anonymous" /> */}
+        <link rel="preconnect" href={process.env.NEXT_PUBLIC_GRAPHQL_URL} crossOrigin="anonymous" />
         <meta name="theme-color" content="#18181b"></meta>
       </Head>
       <body className="bg-zinc-900 text-zinc-200">

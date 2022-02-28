9fcdaa45aef39e1397d733b9b9d93ce4655a8835
Matt Wright <mattwright3195@gmail.com>
2022-02-27 20:14:18 -0800

    change font display

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -5,11 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /> */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=option"
-          rel="stylesheet"
-        />
+        {/* Use display=option */}
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
         <link rel="preconnect" href={process.env.NEXT_PUBLIC_GRAPHQL_URL} crossOrigin="anonymous" />
         <meta name="theme-color" content="#18181b"></meta>
       </Head>
